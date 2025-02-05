3d26ec53a7c9d42b45b73826311e4d3b0480f3bb
Miguel Vivas <savivdrid7@gmail.com>
2025-02-04 23:44:33 -0300

    Se agrego la funcionalidad del carrito

--- serverExpress/src/routes/cart.router.js
+++ serverExpress/src/routes/cart.router.js
@@ -1,9 +1,41 @@
 import express from "express";
+import CartManager from "../CartManager.js";
 
-const cartRouter = express.Router();
+const cartsRouter = express.Router();
+const cartManager = new CartManager("./serverExpress/src/data/carts.json");
 
-cartRouter.get("/carts", (req, res)=> {
-    res.send({username: "Migue01", password: "micontraseña"});
-})
 
-export default cartRouter
+cartsRouter.post("/", async (req, res) => {
+  try {
+    const newCart = await cartManager.createCart();
+    res.status(201).send(newCart);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
+
+
+cartsRouter.get("/:cid", async (req, res) => {
+  try {
+    const { cid } = req.params;
+    const cart = await cartManager.getCartById(cid);
+    res.status(200).send(cart);
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+});
+
+
+cartsRouter.post("/:cid/product/:pid", async (req, res) => {
+  try {
+    const { cid, pid } = req.params;
+    const { quantity = 1 } = req.body;
+    const updatedCart = await cartManager.addProductToCart(cid, pid, quantity);
+    res.status(200).send(updatedCart);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
+
+
+export default cartsRouter;
