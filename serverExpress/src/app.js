4017a058fe8e50cee33d1d83fbfb60bf1f5d89a6
Miguel Vivas <savivdrid7@gmail.com>
2025-02-16 01:18:21 -0300

    Se empezo a modificar para ver el servidor en el explorador pero da error

--- serverExpress/src/app.js
+++ serverExpress/src/app.js
@@ -1,19 +1,25 @@
 import express from "express";
 import productsRouter from "./routes/products.router.js";
 import cartRouter from "./routes/cart.router.js";
-
-
+import { engine } from "express-handlebars";
+import { Server } from "socket.io";
+import http from "http";
+import viewsRouter from "./routes/views.router.js";
+import path from "path";
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", path.resolve("src/views"));
+
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartRouter);
-
-app.get( "/", (req, res)=> {
-    res.send("Tienda Elixir")
-} )
+app.use("/", viewsRouter);
 
 
-app.listen( 8080, () => console.log("Servidor iniciado en: http://localhost:8080") );
+server.listen( 8080, () => console.log("Servidor iniciado en: http://localhost:8080") );
