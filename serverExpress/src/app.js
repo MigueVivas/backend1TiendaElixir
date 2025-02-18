d351f3344108c2f509c6877f9697e45cb66e803c
Miguel Vivas <savivdrid7@gmail.com>
2025-02-18 00:10:40 -0300

    Se carga 2da preentrega

--- serverExpress/src/app.js
+++ serverExpress/src/app.js
@@ -6,20 +6,41 @@ import { Server } from "socket.io";
 import http from "http";
 import viewsRouter from "./routes/views.router.js";
 import path from "path";
+import ProductManager from "./ProductManager.js";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.engine("handlebars", engine());
+app.engine('handlebars', engine({
+    layoutsDir: path.resolve('src/views/layouts'),
+    defaultLayout: 'main'
+}));
 app.set("view engine", "handlebars");
-app.set("views", path.resolve("src/views"));
+app.set("views", path.resolve("./src/views"));
+
+app.use(express.static("public"))
 
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartRouter);
 app.use("/", viewsRouter);
+
+const productManager = new ProductManager("./src/data/product.json");
+io.on("connection", (socket)=> {
+    console.log("Nuevo usuario conectado!");
+
+    socket.on("newProduct", async(productData) => {
+        try {
+            const newProduct = await productManager.addProduct(productData);
+            io.emit("productAdded", newProduct);
+        } catch (error) {
+            console.error("Error añadiendo producto: ", error.message)
+        }
+    })
+})
 
 
 server.listen( 8080, () => console.log("Servidor iniciado en: http://localhost:8080") );
--- serverExpress/src/routes/views.router.js
+++ serverExpress/src/routes/views.router.js
@@ -1,9 +1,26 @@
 import express from "express";
+import ProductManager from "../ProductManager.js";
 
 const viewsRouter = express.Router();
+const productsManager = new ProductManager("./src/data/product.json");
 
-viewsRouter.get("/", (req, res)=> {
-    res.render("home", {});
+viewsRouter.get("/", async(req, res)=> {
+    try {
+        const products = await productsManager.getProducts();
+        res.render("home", {products});
+        
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+})
+
+viewsRouter.get("/realtimeproducts", async(req, res) => {
+    try {
+        const products = await productsManager.getProducts();
+        res.render("realTimeProducts", { products });
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
 })
 
 export default viewsRouter;
